8246404c420272b829d38a0940ef84433b69b353
Steven Bui <t-stevenbui@microsoft.com>
2026-03-25 09:43:11 -0700

    load from container by blob name

--- libs/azure-storage/langchain_azure_storage/testing/sample_loader.py
+++ libs/azure-storage/langchain_azure_storage/testing/sample_loader.py
@@ -1,1 +1,28 @@
-# hello world!
+import os
+
+from azure.identity import InteractiveBrowserCredential
+
+from langchain_azure_storage.changefeed import main as changefeed_blobs_to_refresh
+from langchain_azure_storage.document_loaders import AzureBlobStorageLoader
+
+# python -m langchain_azure_storage.testing.sample_loader  
+
+def main():
+    # design: use the changefeed to fill blob_names with blobs to refresh
+    blobs_to_refresh = changefeed_blobs_to_refresh()
+
+    loader = AzureBlobStorageLoader(
+        account_url=os.getenv('CONN_STR'),
+        container_name="ACCOUNT_URL",
+        blob_names=blobs_to_refresh,
+        credential=InteractiveBrowserCredential(),
+    )
+
+    # test what is lazy laod returns!
+    for doc in loader.lazy_load():
+        print(doc.page_content)
+    
+    # next step: consider full doc loading in RAG pipeline...
+
+if __name__ == '__main__':
+    main()
